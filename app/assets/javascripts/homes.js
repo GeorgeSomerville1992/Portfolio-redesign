9f4457c8b1ac65ff38da2bfa3e79d79f73b2c086
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-11-03 17:36:54 +0000

    added animation to stack adn icons

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,6 +1,25 @@
 $(function(){
- 
-  
+  animationHover(".tada-animate", 'tada')
+  animationHover(".wobble-animate",'wobble')
+  animationHover(".lightspeed-in-animate",'lightSpeedIn')
+  animationHover(".rotate-github-animate","rotateIn")
+  animationHover(".rotate-linkedin-animate","rotateIn")
+  animationHover(".rotate-tumbler-animate", "rotateIn")
+  animationHover(".flash-animate","flash")
+    function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+      function() {
+        element.addClass('animated ' + animation);
+      },
+      function(){
+        //wait for animation to finish before removing classes
+        window.setTimeout( function(){
+          element.removeClass('animated ' + animation);
+        }, 2000);
+      }
+    );
+  };
 
   function changeText(){
     $("#changeText")
